0a6e1342ea9fcae3c926e780610e60217af58515
Chang Yao Chung <yaochung9744@gmail.com>
2021-10-04 12:47:02 -0700

    Update Map.java

--- Server/Map.java
+++ Server/Map.java
@@ -4,7 +4,7 @@ public class GameMap {
     public Object object[][];
     public int width;
     public int height;
-    public Map(int width, int height){
+    public GameMap(int width, int height){
         object = new Object[width][height];
         this.width = width;
         this.height = height;
