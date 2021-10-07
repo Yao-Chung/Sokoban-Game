e739e5de8ef39a663c5aebf87fe40d786b8d0cf9
LuisHsu <luishsuplus@gmail.com>
2021-10-07 11:23:41 -0700

    Add `targets` attribute to GameMap

--- Server/GameMap.java
+++ Server/GameMap.java
@@ -2,6 +2,7 @@ package Server;
 
 public class GameMap {
     public Object object[][];
+    public Target targets[];
     public int width;
     public int height;
     public GameMap(int width, int height){
