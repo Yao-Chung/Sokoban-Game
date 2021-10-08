bcf20f3ebaa61f56eea5701c21d5ea5c37d2332e
LuisHsu <luishsuplus@gmail.com>
2021-10-07 17:42:23 -0700

    Implement loadMap with Main function for testing

--- Server/Game.java
+++ Server/Game.java
@@ -1,18 +1,72 @@
 package Server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Exception;
+
 class Game {
     public GameMap map;
     public String filename;
 
-    public void loadMap(String filename){
-        // TODO: loadMap
+    public void loadMap(String filename) throws Exception{
+        try {
+            // Set filename
+            this.filename = filename;
+            // Create Reader
+            Scanner scanner = new Scanner(new File(filename));
+            // Setup map
+            Scanner header = new Scanner(scanner.nextLine());
+            int width = header.nextInt();
+            int height = header.nextInt();
+            map = new GameMap(width, height);
+            // Store targets
+            ArrayList<Target> targetList = new ArrayList<Target>();
+            // Read lines
+            for(int y = 0; y < height; ++y) {
+                if(!scanner.hasNextLine()){
+                    throw new Exception("insufficient height when loading map file");
+                }
+                String line = scanner.nextLine();
+                for(int x = 0; x < width; ++x) {
+                    if(x >= line.length()){
+                        throw new Exception("insufficient width when loading map file");
+                    }
+                    switch(line.charAt(x)){
+                        case 'B':
+                            map.object[x][y] = new Box();
+                            map.object[x][y].x = x;
+                            map.object[x][y].y = y;
+                        break;
+                        case 'M':
+                            map.object[x][y] = new Man();
+                            map.object[x][y].x = x;
+                            map.object[x][y].y = y;
+                        break;
+                        case 'T':
+                            targetList.add(new Target());
+                            targetList.get(targetList.size() - 1).x = x;
+                            targetList.get(targetList.size() - 1).y = y;
+                        break;
+                        // TODO: Load wall
+                        default:
+                            map.object[x][y] = null;
+                    }
+                }
+            }
+            map.targets = targetList.toArray(new Target[targetList.size()]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Boolean isWin(){
         // TODO:
+        return false;
     }
 
-    public void restart(){
-        // TODO:
+    public void restart() throws Exception{
+        loadMap(filename);
     }
 }
---
+++ Server/Main.java
@@ -0,0 +1,46 @@
+package Server;
+
+public class Main {
+    private static void print(GameMap map){
+        char[][] graph = new char[map.height][map.width];
+        // Normal objects
+        for(int y = 0; y < map.height; ++y){
+            for(int x = 0; x < map.width; ++x){
+                char output = ' ';
+                if (map.object[x][y] instanceof Man){
+                    output = 'M';
+                }else if (map.object[x][y] instanceof Box){
+                    output = 'B';
+                }
+                graph[y][x] = output;
+            }
+        }
+        // Targets
+        for(Target target: map.targets){
+            if(target.touched){
+                graph[target.y][target.x] = '$';
+            }else{
+                graph[target.y][target.x] = 'T';
+            }
+        }
+        // Output
+        for(char[] row: graph){
+            System.out.println(row);
+        }
+    }
+    public static void main(String[] args) {
+        // Check args
+        if(args.length != 1){
+            System.err.println("Usage: java -cp . Server.Main <map_file>");
+            return;
+        }
+        // Game
+        Game game = new Game();
+        try {
+            game.loadMap(args[0]);
+            print(game.map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
