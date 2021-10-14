3aec842e60b7a70c9f986e31208c72f37f006f69
LuisHsu <luishsuplus@gmail.com>
2021-10-14 03:02:45 +0000

    Rename fields in GameMap
    * `object` -> `objects`
    * `isTarget` -> `targets`

--- server/src/game/GameMap.java
+++ server/src/game/GameMap.java
@@ -1,14 +1,14 @@
 import java.util.HashMap;
 
 public class GameMap {
-    public GameObject object[][];
-    public HashMap<Integer, Target> isTarget;
+    public GameObject objects[][];
+    public HashMap<Integer, Target> targets;
     public int rows;
     public int cols;
     public GameMap(int rows, int cols){
         this.rows = rows;
         this.cols = cols;
-        object = new GameObject[rows][cols];        //initialize to null
-        isTarget = new HashMap<Integer, Target>();
+        objects = new GameObject[rows][cols];        //initialize to null
+        targets = new HashMap<Integer, Target>();
     }
 }
