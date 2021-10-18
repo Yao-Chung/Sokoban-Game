15bc60bd1ded6bea533ba6c7ae8d3ec78f43d94b
LuisHsu <luishsuplus@gmail.com>
2021-10-18 02:42:47 +0000

    Fix name after rename & update index.html

--- server/src/http/Util.java
+++ server/src/http/Util.java
@@ -13,16 +13,16 @@ public class Util {
         for(int r = 0; r < map.rows; ++r){
             result += "[";
             for(int c = 0; c < map.cols; ++c){
-                if (map.object[r][c] instanceof Man){
+                if (map.objects[r][c] instanceof Man){
                     result += "\"@\",";
-                }else if (map.object[r][c] instanceof Box){
+                }else if (map.objects[r][c] instanceof Box){
                     result += "\"$\",";
-                }else if (map.object[r][c] instanceof Wall){
+                }else if (map.objects[r][c] instanceof Wall){
                     result += "\"#\",";
                 }else{
                     int targetKey = r*map.cols+c;
-                    if(map.isTarget.containsKey(targetKey)){
-                        if(map.isTarget.get(targetKey).touched){
+                    if(map.targets.containsKey(targetKey)){
+                        if(map.targets.get(targetKey).touched){
                             result += "\"%\",";
                         }else{
                             result += "\".\",";
